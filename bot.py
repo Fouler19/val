dfbd55d190355f0a67ad5df65fb46b4373ab039c
Fouler19 <sadusolih@gmail.com>
2026-01-11 15:51:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,22 @@
-import telebot
-from database import DatabaseManager
+import cv2
+import numpy as np
+from math import sqrt, ceil, floor
 
-bot = telebot.TeleBot('ВАШ_ТОКЕН')
-db = DatabaseManager('database.db')
+def create_collage(image_paths):
+    images = []
+    for path in image_paths:
+        image = cv2.imread(path)
+        images.append(image)
 
-@bot.message_handler(commands=['rating'])
-def handle_rating(message):
-    res = db.get_rating()
-    res_lines = [f'| @{x[0]:<11} | {x[1]:<11}|\n{"_"*26}' for x in res]
-    res_text = '\n'.join(res_lines)
-    res_text = f'|USER_NAME    |COUNT_PRIZE|\n{"_"*26}\n' + res_text
-    bot.send_message(message.chat.id, res_text)
+    num_images = len(images)
+    num_cols = floor(sqrt(num_images))
+    num_rows = ceil(num_images / num_cols)
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    prize_id = int(call.data)
-    user_id = call.from_user.id
+    collage = np.zeros((num_rows * images[0].shape[0], num_cols * images[0].shape[1], 3), dtype=np.uint8)
 
-    if db.get_winners_count(prize_id) < 3:
-        added = db.add_winner(user_id, prize_id)
-        if added:
-            img_path = f'img/{prize_id}.png'
-            try:
-                with open(img_path, 'rb') as photo:
-                    bot.send_photo(user_id, photo, caption="Поздравляем! Ты получил картинку!")
-            except FileNotFoundError:
-                bot.send_message(user_id, "Ошибка: картинка не найдена!")
-        else:
-            bot.send_message(user_id, "Ты уже получил картинку!")
-    else:
-        bot.send_message(user_id, "К сожалению, ты не успел получить картинку! Попробуй в следующий раз :)")
+    for i, image in enumerate(images):
+        row = i // num_cols
+        col = i % num_cols
+        collage[row*image.shape[0]:(row+1)*image.shape[0], col*image.shape[1]:(col+1)*image.shape[1], :] = image
 
-bot.polling(none_stop=True)
+    return collage
